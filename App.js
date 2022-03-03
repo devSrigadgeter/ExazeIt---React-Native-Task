8bd8e3775dc2089fd97ffab26371528b5a078655
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 20:28:02 +0530

    configured the navigation routes in the app.js file

--- App.js
+++ App.js
@@ -5,13 +5,31 @@
  * @format
  * @flow strict-local
  */
-
+// external imports
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// internal imports
+import WelcomeScreen from './src/Screens/WelcomeScreen';
+import ProfileScreen from './src/Screens/ProfileScreen';
+import ButtonsScreen from './src/Screens/ButtonsScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{title: 'Home'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Buttons" component={ButtonsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
