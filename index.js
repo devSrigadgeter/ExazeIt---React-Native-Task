66b2c5c8a9f08bb8b909a1b9b2abd34777daa807
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 20:16:31 +0530

    Usage of store in index file

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /**
  * @format
  */
-
+// external imports
+import React from 'react';
 import {AppRegistry} from 'react-native';
+
+// redux imports
+import {Provider} from 'react-redux';
+import store from './src/Store';
+
+// internal imports
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const MyNativeApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => MyNativeApp);
