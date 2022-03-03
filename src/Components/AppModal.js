a88db14c39ca74a3ddd08518ea815791b6d48217
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 22:20:19 +0530

    created AppModal component

---
+++ src/Components/AppModal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Modal, StyleSheet, View} from 'react-native';
+
+const AppModal = ({showAlert, children, closeAlert}) => {
+  return (
+    <Modal
+      visible={showAlert}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={closeAlert}>
+      <View style={styles.container}>{children}</View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AppModal;
