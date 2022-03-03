ddee2ac688c73a29cb76cfd8f729f8075ded2874
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 21:49:48 +0530

    Added Avatar reusable component

---
+++ src/Components/Avatar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+
+const Avatar = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('../assets/avatar.jpg')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+});
+
+export default Avatar;
