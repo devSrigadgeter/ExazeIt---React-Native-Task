83689a6118d1b14b00cbe93a955a8cd7403e429d
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 21:19:17 +0530

    Updated WelcomeScreen

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,18 +1,43 @@
 // external imports
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, TextInput} from 'react-native';
 
-const WelcomeScreen = () => {
+// internal imports
+import Screen from '../Components/Screen';
+
+const WelcomeScreen = ({navigation}) => {
+  const [name, setName] = useState('');
   return (
-    <View>
-      <Text style={styles.text}>WelcomeScreen</Text>
-    </View>
+    <Screen style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+        placeholder="Enter Your Name"
+      />
+
+      <Button
+        title="Save"
+        onPress={() => {
+          navigation.navigate('Profile');
+        }}
+      />
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 25,
+  container: {
+    padding: 10,
+    paddingBottom: 30,
+    justifyContent: 'flex-end',
+  },
+  input: {
+    padding: 5,
+    width: '100%',
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cecece',
   },
 });
 
