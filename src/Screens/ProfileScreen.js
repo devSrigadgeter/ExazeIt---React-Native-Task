135f64d0dd9bc3977a93b398957665a7f0dd9c34
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-03-03 21:50:14 +0530

    Updated profile page

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,18 +1,42 @@
 // external imports
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import Avatar from '../Components/Avatar';
 
-const ProfileScreen = () => {
+// internal imports
+import Screen from '../Components/Screen';
+
+const ProfileScreen = ({navigation}) => {
+  const {userName} = useSelector(state => state.auth);
+
   return (
-    <View>
-      <Text style={styles.text}>Profile Screen</Text>
-    </View>
+    <Screen style={styles.container}>
+      <Avatar />
+      <Text style={styles.text}>{userName}</Text>
+      <View style={styles.button}>
+        <Button
+          title="Go to Buttons Page"
+          onPress={() => navigation.navigate('Buttons')}
+        />
+      </View>
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    alignItems: 'center',
+  },
   text: {
-    fontSize: 25,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  button: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 30,
   },
 });
 
